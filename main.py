818ca202bd3fc0eaee844398f5c778e512a511f5
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-02-04 15:44:48 +0300

    add .gitignore, start multifile project

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
+from utility import divisors_list
 
 # MEANS: алфавит, используемый во всей программе
 alph = '!%()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^abcdefghijklmnopqrstuvwxyz|~ЁАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё'
 
 # MEANS: длина алфавита, используемого во всей программе
 a_len = len(alph)
-
-
-def divisors_list(x: int) -> list:
-    """
-    Вычисляет уникальные делители числа
-
-    ARGS:
-        x (int): число
-
-    RETURNS:
-        list: список делителей
-    """
-
-    # MEANS: список делителей
-    d_list: list
-
-    # создание списка делителей перебором до корня
-    d_list = [i for i in range(1, int(x ** 0.5) + 1) if x % i == 0]
-
-    # добавляем делители x // i (кроме случая, когда i равно x // i) в обратном порядке
-    d_list += [x // i for i in reversed(d_list) if i != x // i]
-
-    # возвращаем этот список отсортированным
-    return sorted(d_list)
 
 
 def shuffled_alphabet(key: str, alphabet: str) -> str:
