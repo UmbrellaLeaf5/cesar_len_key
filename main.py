3d73dbbd7cb4a341ed9567bcce261a7d176cb82d
Umbrella_Leaf <krivoruchko.di@phystech.edu>
2024-02-05 14:16:33 +0300

    continue to multifile project + create Enum class for Cryption type

--- main.py
+++ main.py
@@ -1,75 +1,15 @@
 from utility import divisors_list
 from alphabet_shuffle import shuffled_alphabet
+from word_cryption import crypted_word, CryptType
 
 # MEANS: алфавит, используемый во всей программе
 alph = '!%()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^abcdefghijklmnopqrstuvwxyz|~ЁАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяё'
 
-# MEANS: длина алфавита, используемого во всей программе
-a_len = len(alph)
-
 # MEANS: список делителей числа - длины алфавита
-alph_divs = divisors_list(a_len)
+alph_divs = divisors_list(len(alph))
 
 # MEANS: последний делитель из списка делителй длины алфавита
 last_div = alph_divs[len(alph_divs) - 1]
-
-
-def sh_cesarlen(stri, shd, king):
-    new_alph = shuffled_alphabet(king, alph)
-
-    input_len = len(stri)
-
-    if a_len % input_len == 0:
-        stri = ''.join(reversed(stri))
-
-    # MEANS: список делителей числа - длины алфавита
-    alph_d_list = divisors_list(a_len)
-
-    last_alph_len_divisor = (alph_d_list)[
-        len((alph_d_list)) - 1]
-    for i in range(len(alph_d_list)):
-        if input_len < (alph_d_list)[i + int(i != len(alph_d_list) - 1)]:
-            last_alph_len_divisor = (alph_d_list)[
-                i + int(a_len % input_len != 0)]
-            break
-
-    listy = []
-    for i in range(a_len//last_alph_len_divisor):
-        listy.append(
-            ''.join(reversed(new_alph[0 + i * last_alph_len_divisor:last_alph_len_divisor * (i + 1)])))
-    listy.append('')
-    for i in range((len(listy) - 1)//2):
-        if i % 2 != 0:
-            listy[len(listy) - 1] = listy[i]
-            listy[i] = listy[len(listy) - 1 - i]
-            listy[len(listy) - 1 - i] = listy[len(listy) - 1]
-    new_alph = ''
-    for i in range(len(listy) - 1):
-        new_alph += listy[i]
-
-    stri2 = ''
-    if shd == '1':
-        for i in range(input_len):
-            for j in range(a_len):
-                if stri[i] == new_alph[j]:
-                    if j + (input_len % a_len) < a_len:
-                        stri2 += new_alph[j + (input_len % a_len)]
-                    else:
-                        stri2 += new_alph[j +
-                                          (input_len % a_len) - a_len]
-
-    if shd == '0':
-        for i in range(input_len):
-            for j in range(a_len):
-                if stri[i] == new_alph[j]:
-                    if j - (input_len % a_len) > 0:
-                        stri2 += new_alph[j - (input_len % a_len)]
-                    elif (input_len % a_len) == j:
-                        stri2 += new_alph[0]
-                    else:
-                        stri2 += new_alph[a_len -
-                                          (input_len % a_len) + j]
-    return stri2
 
 
 def main():
@@ -107,9 +47,11 @@ def main():
             felper = False
 
     print()
+    flag: CryptType = CryptType.decr
     flack = ''
     while flack != '0':
         if flack == '1':
+            flag = CryptType.encr
             break
         if flack != '':
             print('Enter 0 or 1.')
@@ -117,9 +59,9 @@ def main():
         print('Encryption (Enter 1) or Decryption (Enter 0)?')
         flack = input()
         if flack == 'Encryption' or flack == 'encryption':
-            flack = '1'
+            flag = CryptType.encr
         elif flack == 'Decryption' or flack == 'decryption':
-            flack = '0'
+            flag = CryptType.decr
 
     print()
     print('Enter key:')
@@ -140,7 +82,7 @@ def main():
     result: str = ''
     for i in range(len(listw) - 1):
         if listw[i] != ' ':
-            result += sh_cesarlen(listw[i], flack, kluch)
+            result += crypted_word(listw[i], flag, kluch)
         else:
             result += listw[i]
 
